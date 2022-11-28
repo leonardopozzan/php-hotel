2969e614e7aa38fb9a2283480fb805a0e4e9b038
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-11-28 17:25:53 +0100

    logica più semplice

--- index.php
+++ index.php
@@ -37,22 +37,16 @@ $hotels = [
         'distance_to_center' => 50
     ],
 ];
-if(isset($_GET['parking']) && $_GET['parking'] != '' && isset($_GET['vote']) && $_GET['vote'] != ''){
-    if($_GET['parking'] == 'park'){
-        $hotels = array_filter($hotels, fn($value)=> $value['parking'] && $value['vote'] >= $_GET['vote']);
-    }else{
-        $hotels = array_filter($hotels, fn($value)=> !$value['parking'] && $value['vote'] >= $_GET['vote']);
-    };
-    
-}elseif(isset($_GET['parking']) && $_GET['parking'] != ''){
+if(isset($_GET['parking']) && empty($_GET['parking'])){
     if($_GET['parking'] == 'park'){
         $hotels = array_filter($hotels, fn($value)=> $value['parking']);
     }else{
         $hotels = array_filter($hotels, fn($value)=> !$value['parking']);
     };
-}elseif(isset($_GET['vote']) && $_GET['vote'] != ''){
+};
+if(isset($_GET['vote']) && empty($_GET['vote'])){
     $hotels = array_filter($hotels, fn($value)=> $value['vote'] >= $_GET['vote']);
-}
+};
 ?>
 <!DOCTYPE html>
 <html lang='en'>
